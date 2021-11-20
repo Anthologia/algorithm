c7456268cf97f78a9c0ad896bdae44531c27fe53
Anthologia <pyoungjy@gmail.com>
2021-11-21 02:36:36 +0900

    BOJ 12865.cpp

---
+++ BOJ/12865.cpp
@@ -0,0 +1,42 @@
+int w[101], v[101], dp[100001];
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    for (int i=0; i<n; i++)
+        cin >> w[i] >> v[i];
+    
+    for (int i = 0; i < n; i++) {    
+        for (int j = k; j - w[i] >= 0; j--)
+            dp[j] = max(dp[j - w[i]] + v[i], dp[j]);
+    }
+
+    cout << dp[k];
+    return 0;
+}
+
+
+
+
+// #include <iostream>
+// using namespace std;
+// int w[101], v[101], dp[100001];
+
+// int main() {
+//     int n, k;
+//     cin >> n >> k;
+    
+//     for (int i=0; i<n; i++)
+//         cin >> w[i] >> v[i];
+    
+//     for (int i = 0; i < n; i++) {    
+//         for (int j = k; j - w[i] >= 0; j--)
+//             dp[j] = max(dp[j - w[i]] + v[i], dp[j]);
+//     }
+
+//     cout << dp[k];
+//     return 0;
+// }
+
+
