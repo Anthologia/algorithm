ae172ded5392ebd640ebdc65c445390177e35648
Anthologia <pyoungjy@gmail.com>
2022-03-07 22:14:11 +0900

    2022/3/7
    BOJ 1049(그리디), 1106(DP)

---
+++ BOJ/1106.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    static int MAX = 100 * 1000 + 1;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int c = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken());
+        int[] dp = new int[MAX];
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            int promotionPrice = Integer.parseInt(st.nextToken());
+            int client = Integer.parseInt(st.nextToken());
+
+            for (int j = promotionPrice; j < MAX; j++) {
+                dp[j] = Math.max(dp[j - promotionPrice] + client, dp[j]);
+                if (dp[j] >= c) break;
+            }
+        }
+
+        for (int i = 1; i < MAX; i++) {
+            if (dp[i] >= c) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
