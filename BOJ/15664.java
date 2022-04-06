7816aec28ba5eb480345b1d5b7abf0619634a3cb
Anthologia <pyoungjy@gmail.com>
2022-04-06 22:52:46 +0900

    2022/4/6
    BOJ 15663(백트래킹), 15664(백트래킹)

---
+++ BOJ/15664.java
@@ -0,0 +1,65 @@
+// N과 M (10) - S2
+
+import java.io.*;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int n;
+    public static int m;
+    public static int[] arr;
+    public static int[] nums;
+    public static boolean[] visited;
+    public static Set<String> set = new HashSet<>();
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        nums = new int[n];
+        visited = new boolean[n];
+        arr = new int[m];
+
+        st = new StringTokenizer(br.readLine(), " ");
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int cnt) {
+        if (cnt == m) {
+            StringBuilder tmpSb = new StringBuilder();
+            for (int i = 0; i < arr.length; i++) {
+                tmpSb.append(arr[i]).append(" ");
+            }
+
+            if (!set.contains(tmpSb.toString())) {
+                sb.append(tmpSb).append('\n');
+                set.add(tmpSb.toString());
+            }
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                arr[cnt] = nums[i];
+                dfs(cnt + 1);
+                for (int j = i + 1; j < n; j++) {
+                    visited[j] = false;
+                }
+            }
+        }
+    }
+}
